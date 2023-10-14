7d389e072cc5e766bbe268263978c975383083a7
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2023-10-14 20:53:39 +0600

    ReactTable with Dummy Data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,19 +2,196 @@ import Header from "../components/Header";
 import  { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import DataTable from "react-data-table-component";
 
 
 
 const Homepage = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [finishDate, setFinishDate] = useState(new Date());
+  const columns = [
+    {
+      name : "Day",
+      selector: row => row.day,
+      sortable:true
+    },
+      {
+      name : "Temperature",
+       selector: row => row.temp,
+          sortable:true
+    
+    },
+      {
+      name : "Weather",
+         selector: row => row.weather,
+            sortable:true
+    },
+      {
+      name : "Feels Like",
+         selector: row => row.feels,
+            sortable:true
+    },
+      {
+      name : "Wind",
+         selector: row => row.wind,
+            sortable:true
+    },
+      {
+      name : "Humidity",
+         selector: row => row.humidity,
+            sortable:true
+    },
+      {
+      name : "Chance",
+         selector: row => row.chance,
+          sortable:true
+    },
+
+  ];
+  const data = [
+    {
+      id:1,
+      day:"Fri \n 13 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"32°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"11%"
+
+    },
+      {
+      id:2,
+      day:"Sat \n 14 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"34°C",
+      wind:"10km/h",
+      humidity:"23%",
+      chance:"21%"
+
+    },
+      {
+      id:3,
+      day:"Sun \n 15 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"36°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"21%"
+
+    },
+      {
+      id:4,
+      day:"Mon \n 16 Oct",
+      temp:"32/25°C",
+      weather:"Sunny",
+      feels:"32°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"21%"
+
+    },
+        {
+      id:1,
+      day:"Fri \n 13 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"32°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"21%"
+
+    },
+      {
+      id:2,
+      day:"Sat \n 14 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"34°C",
+      wind:"10km/h",
+      humidity:"29%",
+      chance:"12%"
+
+    },
+      {
+      id:3,
+      day:"Sun \n 15 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"36°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"19%"
+
+    },
+      {
+      id:4,
+      day:"Mon \n 16 Oct",
+      temp:"32/25°C",
+      weather:"Sunny",
+      feels:"32°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"22%"
+
+    },
+        {
+      id:1,
+      day:"Fri \n 13 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"32°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"21%"
+
+    },
+      {
+      id:2,
+      day:"Sat \n 14 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"34°C",
+      wind:"10km/h",
+      humidity:"29%",
+      chance:"21%"
+
+    },
+      {
+      id:3,
+      day:"Sun \n 15 Oct",
+      temp:"32/25°C",
+      weather:"Mostly Sunny",
+      feels:"36°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"21%"
+
+    },
+      {
+      id:4,
+      day:"Mon \n 16 Oct",
+      temp:"32/25°C",
+      weather:"Hot",
+      feels:"32°C",
+      wind:"10km/h",
+      humidity:"39%",
+      chance:"21%"
+
+    }
+  
+  
+  ]
+
   return (
     <div className="container">
       <Header />
       <div className="flex justify-between items-start">
         {/* Left Section */}
         <div className="mr-4 w-1/4">
-        
+       
       
         </div>
         {/* Right Section */}
@@ -57,6 +234,14 @@ const Homepage = () => {
 
      
         </div>
+      </div>
+      <div>
+        <DataTable className="text-white mt-5"
+        columns={columns}
+        data={data}
+        fixedHeader
+        pagination
+        ></DataTable>
       </div>
     </div>
   );
