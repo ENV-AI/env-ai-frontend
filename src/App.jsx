0de4107498a5029810e99a4167042626d2738b03
masrur-sakib <mashrur333@gmail.com>
2023-10-18 18:56:11 +0600

    configured the routing and linking to different page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
-// import PredictionDetails from "./components/PredictionDetail/PredictionDetails";
+import PredictionDetails from "./components/PredictionDetail/PredictionDetails";
 import "./App.css";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-950">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route
+          path="/prediction-details"
+          element={<PredictionDetails />}
+        ></Route>
+      </Routes>
+      {/* <Homepage /> */}
       {/* <PredictionDetails /> */}
     </div>
   );
--- src/components/Homepage/Header.jsx
+++ src/components/Homepage/Header.jsx
@@ -1,10 +1,15 @@
 import { BiSearch, BiCurrentLocation } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="container py-5 flex justify-between items-center">
       {/* Logo */}
-      <div className="font-bold text-xl text-white hover-pointer">Env AI</div>
+      <div className="font-bold text-xl text-white hover-pointer">
+        <Link className="text-gray-400" to="/">
+          Env AI
+        </Link>
+      </div>
       {/* Search */}
       <div className="h-[40px] w-[30%] px-3 flex justify-start items-center bg-slate-800 border border-slate-400 rounded-full text-white text-sm">
         <BiSearch className="mr-2" />
@@ -15,10 +20,19 @@ const Header = () => {
           placeholder="Search City..."
         />
       </div>
-      {/* Location Button */}
-      <div className="flex justify-center items-center text-white rounded-full w-[150px] py-1 bg-slate-500 text-sm cursor-pointer">
-        <BiCurrentLocation className="mr-2" />
-        Current Location
+      <div className="flex justify-end items-center gap-4">
+        {/* Location Button */}
+        <div className="flex justify-center items-center text-white rounded-full w-[150px] py-1 bg-slate-500 text-sm cursor-pointer">
+          <BiCurrentLocation className="mr-2" />
+          Current Location
+        </div>
+        {/* Prediction Detail */}
+        <Link
+          className="flex justify-center items-center text-white rounded-full w-[150px] py-1 bg-slate-500 text-sm cursor-pointer"
+          to="prediction-details"
+        >
+          Prediction Detail
+        </Link>
       </div>
     </div>
   );
