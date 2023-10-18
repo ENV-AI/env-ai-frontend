35c1fa752620c45df880df1d37d18275991a3a8e
masrur-sakib <mashrur333@gmail.com>
2023-10-18 21:36:14 +0600

    HourlyTemperature section developed.

--- src/components/Homepage/Highlights.jsx
+++ src/components/Homepage/Highlights.jsx
@@ -1,5 +1,9 @@
 const Highlights = () => {
-  return <div className="text-white">Highlights</div>;
+  return (
+    <div className="my-5 text-white p-3 bg-slate-900 rounded-lg">
+      <h1 className="mb-1">Highlights</h1>
+    </div>
+  );
 };
 
 export default Highlights;
